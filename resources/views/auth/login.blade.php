6ad0f1573807295d62490a10b8078a6bd2b18a1c
Richard Felix <richard.felix@wemakecodes.com>
2021-03-22 20:50:43 -0300

    Ajustes de css realizado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
+        <form method="POST" action="{{ route('login') }}" class="mecanix-login">
             @csrf
 
             <div>
