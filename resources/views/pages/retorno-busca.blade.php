72c780a6f7b43f857121b419055fb6deedc91b59
Richard Felix <richard.felix@wemakecodes.com>
2021-03-29 20:25:01 -0300

    Alteraçoes no retorno da busca

--- resources/views/pages/retorno-busca.blade.php
+++ resources/views/pages/retorno-busca.blade.php
@@ -6,41 +6,38 @@
     </x-slot>
 
     <div>
-        <div class="max-w-7xl mx-auto pb-10 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto pb-10 sm:px-6 lg:px-8 col-span-3 sm:col-span-4">
             <div class="mt-5 md:mt-0 md:col-span-2">
-                @if(count($oficinas) > 0)
-                @foreach ($oficinas as $oficina)
-                    <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md mb-3">
-                        <div class="grid grid-cols-6 gap-6">
-                            {{ $oficina->nome_fantasia }}
+                <div class="grid grid-cols-4 gap-4">
+                    <div class="col-span-1 sm:col-span-4 md:col-span-1 lg:col-span-1">
+                        <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md mb-3">
+                            Filtro
                         </div>
                     </div>
-                @endforeach
-                @else
-                <h1>Nenhuma oficina foi encontrada</h1>
-                <a href="{{ url('/') }}">
-                    <button class="btn-primary btn-full btn-busca-oficinas">Voltar ao início</button>
-                </a>
-                @endif
+                    <div class="col-span-3 sm:col-span-4 md:col-span-3 lg:col-span-3">
+                        @if(count($oficinas) > 0)
+                        @foreach ($oficinas as $oficina)
+                        <div class="bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md mb-3">
+                            <div class="grid grid-cols-4">
+                                <div class="col-span-1 sm:col-span-4 md:col-span-1 lg:col-span-1">
+                                    <img src="{{ asset('img/logotipos/'.$oficina->logo) }}" alt="" width="200">
+                                </div>
+                                <div class="col-span-3 sm:col-span-4 md:col-span-3 lg:col-span-3">
+                                    <p class="nome-ret-busca">{{ $oficina->nome_fantasia }}</p>
+                                    <p class="desc-ret-busca">{{ $oficina->cidade .' - '. $oficina->uf }}</p>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                        @else
+                        <h1>Nenhuma oficina foi encontrada</h1>
+                        <a href="{{ url('/') }}">
+                            <button class="btn-primary btn-full btn-busca-oficinas">Voltar ao início</button>
+                        </a>
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </x-app-layout>
-
-@if(isset($_GET['saved']) != null)
-    <script>
-        Swal.fire(
-            'Salvo!',
-            'Sua oficina foi salva!',
-            'success'
-        )
-    </script>
-@elseif(isset($_GET['deleted']) != null)
-    <script>
-        Swal.fire(
-            'Excluído!',
-            'Registro excluído com sucesso!',
-            'success'
-        )
-    </script>
-@endif
